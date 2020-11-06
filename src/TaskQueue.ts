056621390575d6b5240e300a655ff545a906141a
Attawit Kittikrairit <atton16@gmail.com>
2020-11-06 10:05:39 +0700

    patch(Performance): optimize timer usage

--- src/TaskQueue.ts
+++ src/TaskQueue.ts
@@ -29,9 +29,10 @@ export class TaskQueue {
       throw new TaskQueueError('start is called while TaskQueue is running.', 1);
     }
     this.started = true;
-    for (let i = 0; i < this.opt.concurrent; i++) {
-      this.timeouts.push(setInterval(this.process.bind(this), 0));
-    }
+    this.timeouts.push(setInterval(this.process.bind(this), 0));
+    // for (let i = 0; i < this.opt.concurrent; i++) {
+    //   this.timeouts.push(setInterval(this.process.bind(this), 0));
+    // }
   }
 
   shutdown(): void {
@@ -47,15 +48,17 @@ export class TaskQueue {
   }
 
   process(): void {
-    if (!this.queue.length) {
-      return;
-    }
-    const job = this.queue.shift();
-    job.task().catch(() => {
-      if (typeof job.retry === 'number') {
-        const retry = job.retry >= 0 ? job.retry : 0;
-        setTimeout(() => this.push(job), retry);
+    for (let i = 0; i < this.opt.concurrent; i++) {
+      if (!this.queue.length) {
+        break;
       }
-    });
+      const job = this.queue.shift();
+      job.task().catch(() => {
+        if (typeof job.retry === 'number') {
+          const retry = job.retry >= 0 ? job.retry : 0;
+          setTimeout(() => this.push(job), retry);
+        }
+      });
+    }
   }
 }
